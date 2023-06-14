cad1a0a44a96019193f80648205e40dc28efa37b
Kyle Han <KHan@tcelect.net>
2023-06-14 13:11:51 -0400

    Using classes to hide items

--- Plumsail_Json_Code.js
+++ Plumsail_Json_Code.js
@@ -12,6 +12,16 @@
 *                                                                                                      *
 * fd.saved()        the code is executed once the form is submitted                                    *
 *                                                                                                      *
+*
+*   The following events are possible:
+*       fd.beforeCreate()
+*       fd.created()
+*       fd.beforeRender()
+*       fd.rendered()
+*       fd.beforeSave()
+*       fd.saved()
+*
+*
 * The following predefined variables can be utilized in the code:                                      *
 *                                                                                                      *
 * fd    an instance of the current form                                                                *
@@ -19,116 +29,35 @@
 *                                                                                                      *
 *******************************************************************************************************/
 
+/*
+    I've included the following code due to my use of the $ for JQuery. Plumsail natively supports 
+    JQuery and it can be used externally, but this file is apparently loaded before Plumsail calls JQuery.
+    Thus, this is a hacky workaround - I call for JQuery myself.
+
+    The ideal solution would be to figure out how to reference this document after JQuery has loaded, but
+    still retain the fd.rendered() function in this script. Alternatively, load the JSON file another way.
+*/
 var script = document.createElement('script'); 
  
 script.src = '//code.jquery.com/jquery-1.11.0.min.js'; 
 document.getElementsByTagName('head')[0].appendChild(script);
-// Uncomment the code below to start using it in the form.
 
 // ================================================================
 //  EXAMPLE 1: The code is executed right after rendering the form 
 // ================================================================
-
-
-//Populate Data Table from a public file
-
-
-
 fd.rendered(function () {
     async function externalFile() {
     const data = $.get('https://kyleh2420.github.io/test.json');
     return data;
 }
-
-    // The function populates FullName with values from FirstName and LastName
-    function autoPopulateGenInfo() {
-
-        /*
-        fd.field('NameOfGeneralContractor').value = "JTTC";
-        fd.field('AddressOfGeneralContractor').value = "1010 Northern Blvd, Great Neck, NY 11021";
-        fd.field('GeneralContractNum').value = "A-37139";
-        */
-
-        externalFile().then(function(data){
-            console.log("Running Code");
-            /*
-                The following shows two ways of object deconstruction of multiple objects and subarrays
-                Say we are given the following JSON file in "data"
-                            {
-                                "GeneralInformation": {
-                                    "GeneralContractor": "JTTC",
-                                    "GCAddress": "1010 Northern Blvd, Great Neck, NY 11021",
-                                    "ContractNo": "A-37139",
-                                    "FederallyFunded": "Yes"
-                                },
-                                "SQS": {
-                                    "ProposedSub": "IEEE Inc.",
-                                    "Business Address": "600 Circle Road, Stony Brook, NY 11790",
-                                    "CorpOrCoPartner": "Corporation"
-                                }
-                            }
-                We can extract the objects "GeneralInformation" and "SQS" as its own objects like below. 
-                It will then be available for reference as "GeneralInformation" and "SQS"
-                            const {GeneralInformation, SQS} = data;
-                                Use: GeneralInformation.GeneralContractor
-                
-                You can do the same, but rename the variables that would be referenced.
-                The following will now be available for reference as "GI" or "Form1"
-                            const {GeneralInformation: GI, SQS: Form1} = data;
-                                Use: GI.GeneralContractor
-                
-                Alternatively, you could deconstruct a specific object in the code, such as by isolating "GeneralInformation".
-                Thus, you will be able to use the variables "GC", "GCAddy", "CNo" in your code
-                            const{
-                                GeneralInformation: {
-                                    GeneralContractor: GC,
-                                    GCAddress: GCAddy,
-                                    ContractNo:CNo,
-                                },
-                            } = data;
-            */
-            const {GeneralInformation: GI} = data;
-            console.log(GI);
-            console.log(GI.GeneralContractor);
-            
-            fd.field('GCName').value = GI.GeneralContractor;
-            fd.field('GCAddress').value = GI.GCAddress;
-            fd.field('ContractNo').value = GI.ContractNo;
-            fd.field('FederallyFunded').value = GI.FederallyFunded;
-        })
-    }
-    
-    function showHideQ3() {
-        if (fd.field('CorpOrCoPartner').value === 'Corporation'){
-            fd.field('IncorporationDate').hidden = true;
-            fd.field('PresidentsName').hidden = true;
-            fd.field('VicePresidentsName').hidden = true;
-            fd.field('SecretarysName').hidden = true;
-            fd.field('TreasurersName').hidden = true;
-            
-            fd.field('DateOfOrganization').hidden = false;
-            fd.field('CountyClerk').hidden = false;
-            fd.field('Partners').hidden = false;
-        } else /*if (fd.field('CorpOrCoPartner').value === 'Co-partnership')*/ {
-            fd.field('IncorporationDate').hidden = false;
-            fd.field('PresidentsName').hidden = false;
-            fd.field('VicePresidentsName').hidden = false;
-            fd.field('SecretarysName').hidden = false;
-            fd.field('TreasurersName').hidden = false;
-            
-            fd.field('DateOfOrganization').hidden = true;
-            fd.field('CountyClerk').hidden = true;
-            fd.field('Partners').hidden = true;
-        } 
-    }
-    
-
-    //Autopopulate fields with general information
+    //Functions that run initially
     autoPopulateGenInfo();
     //showHideQ3();
     
-    //On Field change
+
+    //Items that change on action
     fd.field('CorpOrCoPartner').$on('change',showHideQ3);
+    fd.field('B1Question').$on('change', toggleScheduleB1);
 });
 
 /*
@@ -154,3 +83,100 @@ fd.saved(function (result) {
 });
 */
 
+
+
+/*
+    The following are user defined functions - functions that help with the functionality of the page
+*/
+function autoPopulateGenInfo() {
+
+    //A Manual approach to populating fields
+    /*
+    fd.field('NameOfGeneralContractor').value = "JTTC";
+    fd.field('AddressOfGeneralContractor').value = "1010 Northern Blvd, Great Neck, NY 11021";
+    fd.field('GeneralContractNum').value = "A-37139";
+    */
+
+    externalFile().then(function(data){
+        console.log("Running Code");
+        /*
+            The following shows two ways of object deconstruction of multiple objects and subarrays
+            Say we are given the following JSON file in "data"
+                        {
+                            "GeneralInformation": {
+                                "GeneralContractor": "JTTC",
+                                "GCAddress": "1010 Northern Blvd, Great Neck, NY 11021",
+                                "ContractNo": "A-37139",
+                                "FederallyFunded": "Yes"
+                            },
+                            "SQS": {
+                                "ProposedSub": "IEEE Inc.",
+                                "Business Address": "600 Circle Road, Stony Brook, NY 11790",
+                                "CorpOrCoPartner": "Corporation"
+                            }
+                        }
+            We can extract the objects "GeneralInformation" and "SQS" as its own objects like below. 
+            It will then be available for reference as "GeneralInformation" and "SQS"
+                        const {GeneralInformation, SQS} = data;
+                            Use: GeneralInformation.GeneralContractor
+            
+            You can do the same, but rename the variables that would be referenced.
+            The following will now be available for reference as "GI" or "Form1"
+                        const {GeneralInformation: GI, SQS: Form1} = data;
+                            Use: GI.GeneralContractor
+            
+            Alternatively, you could deconstruct a specific object in the code, such as by isolating "GeneralInformation".
+            Thus, you will be able to use the variables "GC", "GCAddy", "CNo" in your code
+                        const{
+                            GeneralInformation: {
+                                GeneralContractor: GC,
+                                GCAddress: GCAddy,
+                                ContractNo:CNo,
+                            },
+                        } = data;
+        */
+        const {GeneralInformation: GI} = data;
+        console.log(GI);
+        console.log(GI.GeneralContractor);
+        fd.fields().forEach( el => function(el) {
+            // access data -> title and value
+            console.log(el.title);
+            console.log(el.value);
+        });
+        
+        fd.field('GCName').value = GI.GeneralContractor;
+        fd.field('GCAddress').value = GI.GCAddress;
+        fd.field('ContractNo').value = GI.ContractNo;
+        fd.field('FederallyFunded').value = GI.FederallyFunded;
+    })
+}
+
+function showHideQ3() {
+    if (fd.field('CorpOrCoPartner').value === 'Corporation'){
+        fd.field('IncorporationDate').hidden = true;
+        fd.field('PresidentsName').hidden = true;
+        fd.field('VicePresidentsName').hidden = true;
+        fd.field('SecretarysName').hidden = true;
+        fd.field('TreasurersName').hidden = true;
+        
+        fd.field('DateOfOrganization').hidden = false;
+        fd.field('CountyClerk').hidden = false;
+        fd.field('Partners').hidden = false;
+    } else /*if (fd.field('CorpOrCoPartner').value === 'Co-partnership')*/ {
+        fd.field('IncorporationDate').hidden = false;
+        fd.field('PresidentsName').hidden = false;
+        fd.field('VicePresidentsName').hidden = false;
+        fd.field('SecretarysName').hidden = false;
+        fd.field('TreasurersName').hidden = false;
+        
+        fd.field('DateOfOrganization').hidden = true;
+        fd.field('CountyClerk').hidden = true;
+        fd.field('Partners').hidden = true;
+    } 
+}
+
+function toggleScheduleB1() {
+    if(fd.field('B1Question').value === 'I need to fill out Schedule B1'){
+        $('ScheduleB1Class').attr('style', 'display:block;');
+    }
+}
