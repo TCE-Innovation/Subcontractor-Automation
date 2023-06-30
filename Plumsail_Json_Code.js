75da92c2aa1bcde8dba7bea3b1b4ea6eee614e1a
Kyle Han <KHan@tcelect.net>
2023-06-30 10:54:02 -0400

    Debugging new way to fetch data

--- Plumsail_Json_Code.js
+++ Plumsail_Json_Code.js
@@ -122,7 +122,7 @@ function externalFile() {
         "Contract": contractNumber
     }
 
-    fetchJSONData(infoToSend)
+    return fetchJSONData(infoToSend)
     .then(json => {
         console.log(json);
         return json
@@ -173,29 +173,33 @@ function autoPopulateGenInfo() {
         fd.field(el.internalName).disabled = false;
     });
 
-    data = externalFile();
-    let editable = [];
-    try{const {EditableItems: editableItems} = data;
-        editable = editableItems;
-    }
-    catch(err) {
-        console.log("There is nothing editable in this document");
-    }
-    //Finally, we will fill each key in
-    Object.entries(data).forEach(el => {
-        const [elKey, elValue] = el;
-        try{
-            fd.field(elKey).value = elValue;
-            if (!editable.includes(elKey) && elKey !== "EditableItems") {
-                fd.field(elKey).disabled = true;
-            }
+    externalFile().then(function(data){
+
+        let editable = [];
+        try{const {EditableItems: editableItems} = data;
+            editable = editableItems;
         }
         catch(err) {
-            console.log("Failed Autofill Key: " + elKey + ". Value: "+ elValue);
-            console.log("Does editable include elKey?: " + editable.includes(elKey));
+            console.log("There is nothing editable in this document");
         }
-        
-    });
+
+        //Finally, we will fill each key in
+        Object.entries(data).forEach(el => {
+            const [elKey, elValue] = el;
+
+            try{
+                fd.field(elKey).value = elValue;
+                if (!editable.includes(elKey) && elKey !== "EditableItems") {
+                    fd.field(elKey).disabled = true;
+                }
+            }
+            catch(err) {
+                console.log("Failed Autofill Key: " + elKey + ". Value: "+ elValue);
+                console.log("Does editable include elKey?: " + editable.includes(elKey));
+            }
+            
+        });
+    })
 }
 /*
     This function will toggle all the disappearing/conditional fields and update every them every time 
