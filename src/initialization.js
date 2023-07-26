603c297adc5b7df7b83e66ebaaefb9fb0e493818
Kyle Han <KHan@tcelect.net>
2023-07-26 14:52:10 -0400

    Changing initialization to API based submitting

--- src/initialization.js
+++ src/initialization.js
@@ -51,3 +51,73 @@ function autoPopulateGenInfo() {
     });
     */
 }
+
+let sendData = {
+    url: "https://prod-76.westus.logic.azure.com:443/workflows/34545c436ff04f18a535e11258c53ad7/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=QaY6UKMxwRLFyre2HKECVA9N8c_3k2WYfX5ZuYYngrg",
+
+    /*
+    +-------------------------------------------------------------------------------------------+
+    |                                                                                           |
+    |      This function will send the data provided to the url provided as POST request.       |
+    |I chose POST over GET since a body with information could be included at all times in POST.|
+    |                                                                                           |
+    +-------------------------------------------------------------------------------------------+
+    */
+    interactWithAPI: function (data, url) {
+        const headers = {
+          'Content-Type': 'application/json'
+        };
+      
+        const options = {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(data)
+        };
+      
+        return fetch(url, options)
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Error: ' + response.status);
+                }
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+    },
+    getFormData: function() {
+        var formData = fd.data();
+        var dtEmail = this.extractData(listOfEmails, "email");
+        var dtCompany = this.extractData(listOfEmails, "company");
+        delete formData.listOfEmails;
+
+        formData.emailList = dtEmail;
+        formData.companyList = dtCompany;
+
+        return formData;
+    },
+    init: function() {
+        this.interactWithAPI(getFormData(), url);
+    },
+    /*
+    +-----------------------------------------------------------------------------------------------+
+    |                                                                                               |
+    |This function will extract all the values in a column in a data table and return it as an array|
+    |                                                                                               |
+    +-----------------------------------------------------------------------------------------------+
+    */
+    extractData: function(dt, col) {
+        returnArray = [];
+        
+        fd.control(dt).value.forEach(row => {
+            returnArray.push(row[col]);
+        });
+        return returnArray;
+    }
+};
+
+fd.beforeSave(function() {
+    sendData.init();
+    throw new Error("Preventing you from submitting");
+});
