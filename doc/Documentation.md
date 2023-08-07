f25186e13fc7e56795566c93f94c7789eeeb6a76
Kyle Han <KHan@tcelect.net>
2023-08-07 10:08:10 -0400

    Added nea items to test dropdown feature

--- doc/Documentation.md
+++ doc/Documentation.md
@@ -1,2 +1,6 @@
 # This is a title
 Hello World
+
+This is the regex pattern to extract all the internal names from the plumsail form:
+(?<="_internalName": ).*(?=,)
+
--- src/correction.js
+++ src/correction.js
@@ -12,7 +12,7 @@
 +-------------------------------------------------------------------------------------------------------------+
 */
 fd.rendered(function() {
-    disableLastColumn();
+    dataTableFunctions.disableLastColumn();
 
     async function externalFile() {
         urlOfJSON = "https://tce-innovation.github.io/Subcontractor-Automation/data/correctionData.json";
@@ -26,10 +26,10 @@ fd.rendered(function() {
             fd.control('DataTable1').$on('edit', function(e) {
                 //console.log(e);
                 if (e.column.field === 'Column1') {
-                    populateColumn(e.widget, e.model.Column1, jsonData.MTAForms);
+                    dataTableFunctions.dataTableFunctions.populateColumn(e.widget, e.model.Column1, jsonData.MTAForms);
                 }
                 if (e.column.field === 'Column2') {
-                    populateColumn(e.widget, e.model.Column2, jsonData.MTAForms[e.model.Column1]);
+                    dataTableFunctions.populateColumn(e.widget, e.model.Column2, jsonData.MTAForms[e.model.Column1]);
                 }
             });
             
@@ -43,19 +43,33 @@ fd.rendered(function() {
                         console.log(jsonData.MTAForms[value[i].Column1]);
                     }
                 }
-                console.log(checkColumn3Filled());
+                console.log(dataTableFunctions.checkColumn3FIlled());
             });
         })
         .catch(error => {
             console.error("Error fetching the JSON data:", error);
         });
     }
+    
+
+    /*
     data = {}
     externalFile().then(function (jsonData) {
         data = jsonData;
     })
-    
+    */
+
+    //At the same time, set up the event listeners for the dropdown values to change its values whenever the user clicks it.
+    fd.field('dd.GI.contractNo').$on('edit', function(value) {
+        fd.field('dd.GI.contractNo').widget.dataSource.data(dataHandling.getContracts());
+    })
+
+    fd.field('dd.GI.subcontractorName').$on('edit', function(value) {
+        fd.field('dd.GI.subcontractorName').widget.dataSource.data(dataHandling.getSubcontractors());
+    })
+
 });
+
 /*
 +-------------------------------------------------------------------------------------------------------------------------+
 |                                                                                                                         |
@@ -75,142 +89,176 @@ fd.rendered(function() {
 +-------------------------------------------------------------------------------------------------------------------------+
 */
 fd.beforeSave(function() {
-    if (checkColumn3Filled()) {
-        console.log(checkColumn3Filled());
+    if (dataTableFunctions.checkColumn3FIlled()) {
+        console.log(dataTableFunctions.checkColumn3FIlled());
         throw new Error("Your form is not completed - there are still missing components");
     }
     
-    url = "https://prod-76.westus.logic.azure.com:443/workflows/34545c436ff04f18a535e11258c53ad7/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=QaY6UKMxwRLFyre2HKECVA9N8c_3k2WYfX5ZuYYngrg";
-    formData = fd.data();
-    dataToSend = {};
-    dataToSend["t.GI.subcontractorName"] = formData["t.GI.subcontractorName"];
-    dataToSend["mt.GI.contractNo"] = formData["mt.GI.contractNo"];
-    console.log(dataToSend);
     
-    formToCorrect = extractData("DataTable1", "Column1");
-    questionsToCorrect = extractData("DataTable1", "Column2");
-
-    bodyOfEmail = [];
-    emptyString = "";
-    for (var i = 0; i < formToCorrect.length; i++) {
-        bodyOfEmail.push(formToCorrect[i].concat(' - ',questionsToCorrect[i]));
-        //console.log(bodyOfEmail);
-    }
-    dataToSend.bodyOfEmail = bodyOfEmail;
-    dataToSend.editableItems = extractData("DataTable1", "Column3");
-    
-    
-    dataToSend.email = extractData("dt.email", "Email");
-    console.log(dataToSend);
-    
-    interactWithAPI(dataToSend, url);
-    throw new Error("Preventing you from submitting");
 });
 
+let dataHandling = {
+    getURL: "https://prod-39.westus.logic.azure.com:443/workflows/e79e3cfdbbed4570a579427a6472f6a7/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=ynEJekS_a33ZhHDPGbDNNUqVqMPjcqk90FZgdp-HmLo",
+    submitURL: "https://prod-76.westus.logic.azure.com:443/workflows/34545c436ff04f18a535e11258c53ad7/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=QaY6UKMxwRLFyre2HKECVA9N8c_3k2WYfX5ZuYYngrg",
+    //getContracts returns an array of the "titles" of all the folders given via a power automate HTTPS flow
+    getContracts: function() {
+        dataToSend = {
+            "getContractNum": "true",
+            "getSubcontractors": ""
+        }
+        responseObject = this.interactWithAPI(dataToSend, getURL);
+        console.log(responseObject);
+    },
 
-/*
-+-----------------------------------------------------------------------------------------------+
-|                                                                                               |
-|This function will extract all the values in a column in a data table and return it as an array|
-|                                                                                               |
-+-----------------------------------------------------------------------------------------------+
-*/
-function extractData(dt, col) {
-    returnArray = [];
+    getSubcontractors: function() {
+        dataToSend = {
+            "getContractNum": "false",
+            "getSubcontractors": "R-33333"
+        }
+        responseObject = this.interactWithAPI(dataToSend, getURL);
+        console.log(responseObject);
+
+    },
+    submitData: function() {
+        formData = fd.data();
+        dataToSend = {};
+        dataToSend["t.GI.subcontractorName"] = formData["t.GI.subcontractorName"];
+        dataToSend["mt.GI.contractNo"] = formData["mt.GI.contractNo"];
+        console.log(dataToSend);
+        
+        formToCorrect = dataTableFunctions.extractData("DataTable1", "Column1");
+        questionsToCorrect = dataTableFunctions.extractData("DataTable1", "Column2");
     
-    fd.control(dt).value.forEach(row => {
-        returnArray.push(row[col]);
-    });
-    return returnArray;
-}
-/*
-+-------------------------------------------------------------------------------------------+
-|                                                                                           |
-|      This function will send the data provided to the url provided as POST request.       |
-|I chose POST over GET since a body with information could be included at all times in POST.|
-|                                                                                           |
-+-------------------------------------------------------------------------------------------+
-*/
-function interactWithAPI(data, url) {
-    const headers = {
-      'Content-Type': 'application/json'
-    };
-  
-    const options = {
-      method: 'POST',
-      headers: headers,
-      body: JSON.stringify(data)
-    };
-  
-    return fetch(url, options)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error('Error: ' + response.status);
+        bodyOfEmail = [];
+        emptyString = "";
+        for (var i = 0; i < formToCorrect.length; i++) {
+            bodyOfEmail.push(formToCorrect[i].concat(' - ',questionsToCorrect[i]));
+            //console.log(bodyOfEmail);
         }
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
+        dataToSend.bodyOfEmail = bodyOfEmail;
+        dataToSend.editableItems = dataTableFunctions.extractData("DataTable1", "Column3");
+        
+        
+        dataToSend.email = dataTableFunctions.extractData("dt.email", "Email");
+        console.log(dataToSend);
+        
+        this.interactWithAPI(dataToSend, submitURL);
+        throw new Error("Preventing you from submitting");
+    },
+    /*
+    +-------------------------------------------------------------------------------------------+
+    |                                                                                           |
+    |      This function will send the data provided to the url provided as POST request.       |
+    |I chose POST over GET since a body with information could be included at all times in POST.|
+    |                                                                                           |
+    +-------------------------------------------------------------------------------------------+
+    */
+    interactWithAPI: function(data, url) {
+        const headers = {
+        'Content-Type': 'application/json'
+        };
+    
+        const options = {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(data)
+        };
+    
+        return fetch(url, options)
+        .then(response => {
+            if (response.ok) {
+            return response.json();
+            } else {
+            throw new Error('Error: ' + response.status);
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    }
 }
 
-/*
-+--------------------------------------------------------------------------------------------------------+
-|                                                                                                        |
-|                 This function, checkColumn3Filled(), iterates through the data table                   |
-|"DataTable1" and counts the number of rows where the value in "Column3" is undefined (i.e., not filled).|
-|                                                                                                        |
-|                 returns {number} The count of rows with undefined values in "Column3".                 |
-|                                                                                                        |
-|                                                                                                        |
-+--------------------------------------------------------------------------------------------------------+
-*/
-function checkColumn3Filled() {
-    //This function should loop through the data table and return 1 if the column something is undefined (ie. not filled)
-    //Thus, lets count the number of undefineds. If it exceeds 1, then we have an empty row, and we should return the number of undefineds. Otherwise, return 0
-    var i = 0;
-    fd.control("DataTable1").value.forEach(row => {
-        if (row.Column3 === undefined) {
-            i++;
-        }
-    });
-    return i;
-}
 
-/*
-+---------------------------------------------------------------------------------------------------------------+
-|                                                                                                               |
-|This helper function, populateColumn(), is designed to populate the dropdown of a specific cell in a datatable.|
-|                                                                                                               |
-|                    @param {any} widget - The widget (dropdown) to be populated with data.                     |
-|                                                                                                               |
-|           @param {any} value - The value to be selected in the dropdown (if it exists in the data).           |
-|                                                                                                               |
-|          @param {Array} arrayName - The array containing the data to populate the dropdown options.           |
-|                                                                                                               |
-|                           @returns {void} This function does not return any value.                            |
-|                                                                                                               |
-+---------------------------------------------------------------------------------------------------------------+
-*/
-function populateColumn(widget, value, arrayName) {
-    widget.setDataSource({
-        data: Object.keys(arrayName)
-    });
-    widget.value(value);
-}
 
-/*
-+-------------------------------------------------------------------------------------+
-|                                                                                     |
-|               This function, disableLastColumn(), is used to disable                |
-|the editing capability of the last column ('Column3') in the data table "DataTable1".|
-|                                                                                     |
-|              @returns {void} This function does not return any value.               |
-|                                                                                     |
-+-------------------------------------------------------------------------------------+
-*/
-function disableLastColumn() {
-    const premiumColumn = fd.control("DataTable1").columns.find(c => c.field === 'Column3');
-    premiumColumn.editable = () => false;
+
+
+
+
+let dataTableFunctions = {
+
+    /*
+    +--------------------------------------------------------------------------------------------------------+
+    |                                                                                                        |
+    |                 This function, dataTableFunctions.checkColumn3FIlled(), iterates through the data table                   |
+    |"DataTable1" and counts the number of rows where the value in "Column3" is undefined (i.e., not filled).|
+    |                                                                                                        |
+    |                 returns {number} The count of rows with undefined values in "Column3".                 |
+    |                                                                                                        |
+    |                                                                                                        |
+    +--------------------------------------------------------------------------------------------------------+
+    */
+    checkColumn3FIlled: function() {
+        //This function should loop through the data table and return 1 if the column something is undefined (ie. not filled)
+        //Thus, lets count the number of undefineds. If it exceeds 1, then we have an empty row, and we should return the number of undefineds. Otherwise, return 0
+        var i = 0;
+        fd.control("DataTable1").value.forEach(row => {
+            if (row.Column3 === undefined) {
+                i++;
+            }
+        });
+        return i;
+    },
+
+    /*
+    +---------------------------------------------------------------------------------------------------------------+
+    |                                                                                                               |
+    |This helper function, dataTableFunctions.populateColumn(), is designed to populate the dropdown of a specific cell in a datatable.|
+    |                                                                                                               |
+    |                    @param {any} widget - The widget (dropdown) to be populated with data.                     |
+    |                                                                                                               |
+    |           @param {any} value - The value to be selected in the dropdown (if it exists in the data).           |
+    |                                                                                                               |
+    |          @param {Array} arrayName - The array containing the data to populate the dropdown options.           |
+    |                                                                                                               |
+    |                           @returns {void} This function does not return any value.                            |
+    |                                                                                                               |
+    +---------------------------------------------------------------------------------------------------------------+
+    */
+    populateColumn: function(widget, value, arrayName) {
+        widget.setDataSource({
+            data: Object.keys(arrayName)
+        });
+        widget.value(value);
+    },
+
+    /*
+    +-------------------------------------------------------------------------------------+
+    |                                                                                     |
+    |               This function, disableLastColumn(), is used to disable                |
+    |the editing capability of the last column ('Column3') in the data table "DataTable1".|
+    |                                                                                     |
+    |              @returns {void} This function does not return any value.               |
+    |                                                                                     |
+    +-------------------------------------------------------------------------------------+
+    */
+    disableLastColumn: function() {
+        const premiumColumn = fd.control("DataTable1").columns.find(c => c.field === 'Column3');
+        premiumColumn.editable = () => false;
+    },
+
+    /*
+    +-----------------------------------------------------------------------------------------------+
+    |                                                                                               |
+    |This function will extract all the values in a column in a data table and return it as an array|
+    |                                                                                               |
+    +-----------------------------------------------------------------------------------------------+
+    */
+    extractData: function(dt, col) {
+        returnArray = [];
+        
+        fd.control(dt).value.forEach(row => {
+            returnArray.push(row[col]);
+        });
+        return returnArray;
+    }
 }
