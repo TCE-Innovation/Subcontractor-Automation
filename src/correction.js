c56b1801a0124fd0d9c397ee14b03514b410cb4e
Kyle Han <KHan@tcelect.net>
2023-08-07 10:44:34 -0400

    Getting contract number to work

--- src/correction.js
+++ src/correction.js
@@ -60,9 +60,9 @@ fd.rendered(function() {
     */
 
     //At the same time, set up the event listeners for the dropdown values to change its values whenever the user clicks it.
-    dataHandling.getContracts(function (value) {
+    //fd.field('dd.GI.contractNo').widget.dataSource.data(value);
+    dataHandling.getContracts(value => {
         console.log(value);
-        fd.field('dd.GI.contractNo').widget.dataSource.data(value);
     });
 
     fd.field('dd.GI.contractNo').$on('change', function(value) {
